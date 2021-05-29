674f44d14096b08f5dbd4fd6ac8c352b23fd295d
lucasrodriguesdelira <54848684+lucasrodriguesdelira@users.noreply.github.com>
2021-05-29 09:46:18 -0300

    Update valida.sh

--- valida.sh
+++ valida.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 URL=$(cat sites.txt)
-
 echo "<!DOCTYPE html>"
 echo "<html lang='pt-br'>"
 echo "   <head>"
@@ -13,23 +11,29 @@ echo "      <font size='5' face='Tahoma, Verdana, Arial, Helvetica, sans-serif' 
 echo "         <h3>Validações de WebSites</h3>"
 echo "      </font>"
 echo "      <div align='center'>"
-echo "        <Table border=0 cellpadding=0 cellspacing=8>"
+echo "        <Table border=0 cellpadding=1 cellspacing=5>"
 echo "            <TR bgcolor=red align=center>"
-echo "                    <TD><FONT COLOR=white>WEBSITE</TD>"
+echo "                    <TD><FONT COLOR=white>URL</TD>"
+echo "                    <TD><FONT COLOR=white>STATUS CODE</TD>"
 echo "                    <TD><FONT COLOR=white>STATUS</TD>"
-echo "                    <TD><FONT COLOR=white>TEMPO</TD>"
+echo "                    <TD><FONT COLOR=white>TIME TAKEN</TD>"
+echo "                    <TD><FONT COLOR=white>DATA</TD>"
 echo "                </TR>"
-for	i in $URL;do
+for i in $URL; do
+DATA=$(date)
 echo "<TD align=center>" $i "</TD>"
 http_resp=`curl $i -o /dev/null -s -w "%{http_code} %{time_total}"`
-http_response=`echo $http_resp | awk -F" " '{ print $1}'`
+http_code=`echo $http_resp | awk -F" " '{ print $1}'`
 http_time=`echo $http_resp | awk -F" " '{ print $2}'`
-if [ $http_response == "200" ] || [ $http_response == "301" ]; then
-echo "<TD align=center bgcolor=gree><FONT COLOR=white> $http_response </TD>"
+if [ $http_code == "200" ] || [ $http_code == "301" ]; then
+echo "                  <TD align=center> $http_code </TD>"
+echo "                  <TD align=center bgcolor=green><FONT COLOR=white> RUNNING </TD>"
 else
-echo "<TD align=center bgcolor=red><FONT COLOR=white> $http_response </TD>"
+echo "                  <TD align=center> $http_code </TD>"
+echo "                  <TD align=center bgcolor=red><FONT COLOR=white> NOK </TD>"
 fi
-echo "<TD align=center> $http_time </TD></TR>"
+echo "                  <TD align=center> $http_time </TD>"
+echo "                  <TD align=center>" $DATA "</TD></TR>"
 done
 echo "            </TR>"
 echo "        </Table>"
